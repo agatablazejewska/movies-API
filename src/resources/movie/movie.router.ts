41bd2ffffe4d092f6b1204298054a973d8457ba8
Agata Sokołowska <agata.blazejewska0@gmail.com>
2021-06-23 15:39:46 +0200

    Applied transformer and validator as middleware

--- src/resources/movie/movie.router.ts
+++ src/resources/movie/movie.router.ts
@@ -1,5 +1,7 @@
 import Router from 'express-promise-router';
 import { movieController } from '../../bootstrap';
+import MovieTransformer from './middleware/movieTransformer';
+import MovieValidator from './middleware/movieValidator';
 
 const router = Router();
 
@@ -7,6 +9,6 @@ router.get('/', async (req, res) => await movieController.getRandom(req, res));
 router.get('/:durationFrom/:durationTo', async (req, res) => await movieController.getByDuration(req, res));
 router.get('/:genres', async (req, res) => await movieController.getByGenres(req, res));
 router.get('/:genres/:durationFrom/:durationTo', async (req, res) => await movieController.getByGenresAndDuration(req, res));
-router.post('/', async (req, res) => await movieController.createMovie(req, res));
+router.post('/',MovieTransformer.transformBodyToCreateMovieDto, MovieValidator.validateCreateMovie, async (req, res) => await movieController.createMovie(req, res));
 
 export default router;
