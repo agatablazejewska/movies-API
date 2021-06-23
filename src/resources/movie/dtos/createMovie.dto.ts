83e751a05c873980c70a911625172d6e3e0673db
Agata Sokołowska <agata.blazejewska0@gmail.com>
2021-06-23 15:34:24 +0200

    Splitted dtos from model, as model requires some properties as strings and dtos need them as numbers.
    Simplified validations - removed error messages from them

--- src/resources/movie/dtos/createMovie.dto.ts
+++ src/resources/movie/dtos/createMovie.dto.ts
@@ -1,47 +1,33 @@
-import { Type } from 'class-transformer';
+import { Expose, Transform, Type } from 'class-transformer';
 import {
-    IsEnum,
-    IsInt, IsNotEmpty, IsNumber, IsNumberString, IsOptional, IsPositive, IsString, IsUrl,
-    Length,
-    MaxLength,
-    MinLength,
+    IsDefined, IsString, IsNotEmpty, MaxLength, IsNumberString, IsPositive, IsEnum,
+    IsOptional, IsUrl, Length, ValidateIf, Matches, IsInt, Min, Max,
 } from 'class-validator';
-import { IMovie } from '../movie.model';
+import { GENRES } from '../../../shared/enums/genres';
+import { IMovieDto } from './IMovieDto';
 
-export interface ICreateMovieDto extends IMovie {}
+export interface ICreateMovieDto extends IMovieDto {}
 
 export default class CreateMovieDto implements ICreateMovieDto {
-    @IsString({
-        message: 'Title should be a string',
-    })
-    @IsNotEmpty({
-        message: `Title can't be an empty string`,
-    })
-    @MaxLength(255, {
-        message: 'Title is too long. Maximal length is $constraint1 characters, but actual is $value',
-    })
+    @IsDefined()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
     title: string;
 
-    @IsNumberString()
-    @Length(4, 4, {
-        message: 'Year must contain only 4 characters'
-    })
-    @Type(() => Number)
+    @IsInt()
+    @Min(1900)
+    @Max(new Date().getFullYear())
     year: number;
 
-    @IsNumberString()
-    @Type(() => Number)
-    @IsPositive({
-        message: `Runtime can't be a negative value`,
-    })
+    @IsInt()
+    @Min(1)
+    @Max(1000)
+    @IsPositive()
     runtime: number;
 
-    @IsString({
-        message: `Director's name has to be a string`,
-    })
-    @IsNotEmpty({
-        message: `Director's name can't be an empty string`,
-    })
+    @IsString()
+    @IsNotEmpty()
     @MaxLength(255, {
         message: `Director's name is too long. Maximal length is $constraint1 characters, but actual is $value`,
     })
@@ -56,27 +42,17 @@ export default class CreateMovieDto implements ICreateMovieDto {
     @IsString({
         message: `Actors have to be represented a s string`,
     })
-    @IsNotEmpty({
-        message: `Actors can't be an empty string`,
-    })
+    @IsNotEmpty()
     actors?: string;
 
     @IsOptional()
-    @IsString({
-        message: `Plot has to be a string`,
-    })
-    @IsNotEmpty({
-        message: `Plot can't be an empty string`,
-    })
+    @IsString()
+    @IsNotEmpty()
     plot?: string;
 
     @IsOptional()
-    @IsNotEmpty({
-        message: `Url of the poster can't be an empty string`,
-    })
-    @IsUrl({}, {
-        message: `Poster's url has an invalid format`,
-    })
+    @IsNotEmpty()
+    @IsUrl()
     posterUrl?: string;
 
     constructor(createMovieDto: ICreateMovieDto) {
