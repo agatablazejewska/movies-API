98213461befb5e36fd1dc86450dc32a221105cf5
Agata Sokołowska <agata.blazejewska0@gmail.com>
2021-06-24 22:57:28 +0200

    Added envtest file and added it to git, to avoid unnecessary chaos
    If this app would ever be used by other people, having .envtest in git repo makes setup much easier

--- __test__/globalSetup.ts
+++ __test__/globalSetup.ts
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, `./env.test`) });
+require('dotenv').config({ path: path.resolve(__dirname, `../envtest`) });
 
 const globalSetup = () => {
     return;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import dotenv from 'dotenv';
-dotenv.config({ path: path.resolve(__dirname, `../.env`) });
+dotenv.config({ path: path.resolve(__dirname, `../.env${process.env.NODE_ENV ?? ''}`) });
 import express from 'express';
 import movieRouter from './resources/movie/movie.router';
 
@@ -22,6 +22,7 @@ app.all('*', (req, res) => {
 
 app.use((error, req, res, next) => {
     const statusCode = error.code ? error.code : 500;
+    const message = error.message;
     res.status(statusCode).json({ error: error.message });
 });
 
