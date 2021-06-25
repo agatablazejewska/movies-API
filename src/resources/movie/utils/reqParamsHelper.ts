160f725cc6cb4060d7f7f32d01d123fa675e32b3
Agata Sokołowska <agata.blazejewska0@gmail.com>
2021-06-25 11:44:54 +0200

    Added parsing to int and error handling for this situation

--- src/resources/movie/utils/reqParamsHelper.ts
+++ src/resources/movie/utils/reqParamsHelper.ts
@@ -1,14 +1,24 @@
 import { Request } from 'express';
 import { GENRES } from '../../../shared/enums/genres';
+import BadRequestError from './badRequestError';
 
 export const convertGenresParamToArr = (req: Request): GENRES[] => {
     const genres = req.params.genres.split(',');
     return genres as GENRES[];
 };
 
-export const getDurationParams = (req): { from: number; to: number } => {
-    const from = req.params.durationFrom;
-    const to = req.params.durationTo;
+export const getDurationParams = (req, next): { from: number; to: number } => {
+    try {
+        const from = parseInt(req.params.durationFrom);
+        const to = parseInt(req.params.durationTo);
 
-    return { from, to };
+        return { from, to };
+    } catch {
+        const error =  new BadRequestError(
+            400,
+            'Duration must be an integer number',
+        )
+
+        next(error);
+    }
 }
